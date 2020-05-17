1602c79aecb55ee949ad438a076e4f8f44289d58
MarcinBamburski1987 <marcin2511@op.pl>
2020-05-17 23:33:17 +0200

    udemy - [ngClass] and ngFor and odd index used to print elements with class acent-4 or acent-5 (difrent colors) depending ond index odd or even. If odd is true then acent-4 else acent 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Quotation } from './models/quotation';
+import { QUOTES } from './models/data-base';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+  quotes: Quotation[] = QUOTES;
 
   onSwitchForm(): void {
     this.showForm = !this.showForm;
