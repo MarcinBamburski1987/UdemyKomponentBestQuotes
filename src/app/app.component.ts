217fb9ee28253af26c2377ef0e6fe71a09894ee5
MarcinBamburski1987 <marcin2511@op.pl>
2020-05-18 22:27:59 +0200

    list quotes is sorted when method addQuotation and addVote is trigged (my idea)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,16 @@ export class AppComponent {
   showForm = false;
   quotes: Quotation[] = QUOTES;
   quotation: Quotation = {author: '', sentence: '', votes: 0};
-
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
   addQuotation() {
     this.quotes.unshift(this.quotation);
     this.quotation = {author: '', sentence: '', votes: 0};
+    this.quotes.sort((a, b) => b.votes - a.votes);
   }
   addVote(quotation: Quotation, value: number) {
     quotation.votes += value;
+    this.quotes.sort((a, b) => b.votes - a.votes);
   }
 }
