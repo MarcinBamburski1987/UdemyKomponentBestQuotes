caa27abcf4f41a83ad80322212176f9eae620b9a
MarcinBamburski1987 <marcin2511@op.pl>
2020-05-31 19:43:54 +0200

    ng g c form --skipTests true so generate new component without test named "form"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { RankingComponent } from './ranking/ranking.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    RankingComponent
+    RankingComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule, FormsModule
